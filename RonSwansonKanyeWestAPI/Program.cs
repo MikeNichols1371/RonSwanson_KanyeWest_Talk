db08a31736d38b604218c2cf40a1628d8c761eca
MikeNichols1371 <mnich13711@gmail.com>
2022-08-02 15:44:42 -0400

    They had a conversation

---
+++ RonSwansonKanyeWestAPI/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+var kanyeURL = "https://api.kanye.rest";
+var ronSwansonUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+var ronSwansonResponse = client.GetStringAsync(ronSwansonUrl).Result;
+
+var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+var ronSwansonQuote = JArray.Parse(ronSwansonResponse).ToString().Replace('[',' ').Replace(']', ' ');
+
+Console.WriteLine(kanyeQuote);
+Console.WriteLine(ronSwansonQuote);
+
+
