bdd41304548664d5540ee5214139858d5331ffb3
MikeNichols1371 <mnich13711@gmail.com>
2022-08-03 15:37:12 -0400

    added for loop

--- RonSwansonKanyeWestAPI/Program.cs
+++ RonSwansonKanyeWestAPI/Program.cs
@@ -5,13 +5,19 @@ var client = new HttpClient();
 var kanyeURL = "https://api.kanye.rest";
 var ronSwansonUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
-var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
-var ronSwansonResponse = client.GetStringAsync(ronSwansonUrl).Result;
+for (int i = 0; i < 5; i++)
+{
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+    var ronSwansonResponse = client.GetStringAsync(ronSwansonUrl).Result;
 
-var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
-var ronSwansonQuote = JArray.Parse(ronSwansonResponse).ToString().Replace('[',' ').Replace(']', ' ');
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+    var ronSwansonQuote = JArray.Parse(ronSwansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
 
-Console.WriteLine(kanyeQuote);
-Console.WriteLine(ronSwansonQuote);
+    Console.WriteLine($"Kanye West: {kanyeQuote}");
+    Console.WriteLine($"Ron Swanson: {ronSwansonQuote}");
+    Console.WriteLine();
+    
+}
+
 
 
